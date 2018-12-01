53ad701a9b18b100d4b57531951883e0ce6066b3
Kelvin Tagoe <tagoe.k@gmail.com>
2018-12-01 18:58:36 -0500

    Part 2 - 4 Completed

--- A3Template/sr_container.c
+++ A3Template/sr_container.c
@@ -182,8 +182,8 @@ int main(int argc, char **argv)
      **/
 
         // You code for clone() goes here
-     void* child_stack = malloc(STACK_SIZE) + STACK_SIZE;
-     child_pid = clone( &child_function, child_stack, SIGCHLD | CLONE_NEWNET | CLONE_NEWCGROUP | CLONE_NEWPID | CLONE_NEWIPC | CLONE_NEWNS | CLONE_NEWUTS, (void *) config);
+     void* stack = malloc(STACK_SIZE) + STACK_SIZE;
+     child_pid = clone(&child_function, stack, SIGCHLD | CLONE_NEWNET | CLONE_NEWCGROUP | CLONE_NEWPID | CLONE_NEWIPC | CLONE_NEWNS | CLONE_NEWUTS, config);
 
     /**
      *  ------------------------------------------------------
