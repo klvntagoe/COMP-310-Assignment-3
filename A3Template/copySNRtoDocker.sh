83a6b02aa8a5b9603887322ab013dea3f58344f9
Kelvin Tagoe <tagoe.k@gmail.com>
2018-12-03 17:09:08 -0500

    yeee boiii

---
+++ A3Template/copySNRtoDocker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+NAME="$1"
+
+cp ~/COMP-310-Assignment-3/A3Template/SNR_CONTAINER $NAME:/home;
---
+++ A3Template/cpToDocker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp ~/COMP-310-Assignment-3/A3Template/SNR_CONTAINER JonahContainer:/home;
